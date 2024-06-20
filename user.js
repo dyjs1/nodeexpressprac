de26e95f03f9709ac23c669336c3595811d8a133
dyjs1 <jsyeon@cowave.kr>
2024-06-20 09:50:03 +0900

    주석 추가

--- user.js
+++ user.js
@@ -6,6 +6,48 @@ const { v4: uuidv4 } = require("uuid"); // user pk를 위한 uuid 모듈에서 v
 // **회원가입 / 로그인 api 만들기 ** //
 
 
+
+// ** 요구사항  **//
+
+/*
+## 회원가입 / 로그인 api 만들기
+
+# 회원가입 요구사항
+- id / password로 로그인한다.
+- id는 중복될 수 없다.
+- password 제한조건은 없다.
+- pk는 단순 숫자말고 uuid를 사용해본다. 
+- signup (post) - id, password
+  성공 -> ok:회원가입 성공 status=200
+  실패 -> error:이미 존재하는 아이디 입니다. status=400
+- request -> id password
+- response -> message 성공 + pk,id return
+
+## uuid란?
+
+UUID는 'Universally Unique Identifier'의 약자로 128-bit의 고유 식별자
+다른 고유 ID 생성 방법과 다르게 UUID는 중앙 시스템에 등록하고 발급하는 과정이 없어서 상대적으로 더 빠르고 간단하게 만들 수 있다는 장점
+
+## uuid 사용방법
+
+npm install uuid
+const { v4: uuidv4 } = require("uuid"); // uuid 모듈에서 v4 함수 가져오기
+// UUID 생성
+const userId = uuidv4();
+
+# 로그인
+
+- 존재하는 아이디인지 확인
+- 아이디가 존재한다면 비밀번호가 일치하는지 확인
+- log-in (post) - id, password
+  성공 -> ok:로그인 성공 status=200
+  실패 -> error:이미 존재하는 아이디 입니다. status=400
+  
+- request -> id password
+- response -> message 성공 + id return
+*/
+
+
 // JSON 형식의 요청 본문을 파싱하기 위해 필요
 app.use(express.json());
 
